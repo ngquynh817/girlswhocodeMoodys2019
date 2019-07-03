d6ba04d70d4b8863d02066b08c3e20ca64b23eec
ngquynh817 <52472646+ngquynh817@users.noreply.github.com>
2019-07-03 10:32:47 -0700

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,12 @@
+
+function add (a, b){
+var sum = a+b;
+return sum;
+}
+function whatsMyName(){
+  alert("My name is Quynh");
+  var name = document.getElementById('wname');
+  name.innerHTML = "My name is Quynh";
+  name. style.color= "white" ;
+  name.style.fontsize= "100px"
+}
